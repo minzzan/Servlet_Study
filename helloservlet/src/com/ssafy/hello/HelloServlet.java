94e655974128169d9a109d68dfdaf07f7bc7b7bd
minzzan <kmmjj1106@gmail.com>
2021-09-24 23:56:39 +0900

    servlet files upload

---
+++ helloservlet/src/com/ssafy/hello/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.ssafy.hello;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("<h2>HI MUENGX2</h2>").append(request.getContextPath());
+	}
+
+}
